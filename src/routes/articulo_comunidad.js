f54285de5a2f5de665b8c20b5a1785fce1425089
Crramos <carlos.ramosrial@usp.ceu.es>
2024-11-24 13:39:08 +0100

    cambios

--- src/routes/articulo_comunidad.js
+++ src/routes/articulo_comunidad.js
@@ -1,83 +1,9 @@
-<!DOCTYPE html>
-<html lang="es">
-	<head>
-		<meta charset="utf-8">
-		<title>Artículos Comunidad</title>
-		<meta content="width=device-width, initial-scale=1.0" name="viewport">
+const express = require('express');
+const router = express.Router();
 
-		<!-- Customized Bootstrap Stylesheet -->
-		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
-		<link rel="stylesheet" href="estilos.css">
-		<link rel="stylesheet" href="estilos2.css">
-		<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
-		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
-		<script src="https://use.fontawesome.com/fe459689b4.js"></script>
-	</head>
-	<header>
-		<nav>
-			<ul>
-				<li><a href="index.html">Gymanual</a></li>
-				<li><a href="ejercicios.html">Ejercicios</a></li>
-				<li><a href="conocenos.html">Conócenos</a></li>
-				<li><a href="contacto.html">Contacto</a></li>
-			</ul>
-			<div class="header-right">
-				<div class="search-bar">
-					<input type="text" placeholder="Buscar...">
-					<button type="submit">Buscar</button>
-				</div>
-				<div class="user-profile">
-					<img src="avatar.webp" alt="Foto de perfil" class="profile-pic">
-				</div>
-			</div>
-		</nav>
-	</header>
-	<body>
-		<div class="background-comunidad">
-			<div class="lefted-title">
-				<h2>Categoria</h2>
-			</div>
-			<div class="centered-title">
-				<h2>Título 1</h2>
-			</div>
-			<div class="imagen-articulo">
-				<img src="img5.jpg" alt="Imagen de ejercicio">
-				<div class="star-icon">★</div>
-			</div>
-			<div class="margin-left">
-				<p class="author-name">Usuario 2</p>
-				<div class="reaction-buttons carousel-item">
-					<button class="likeButton reaction-btn">👍 <span class="likeCount">0</span></button>
-					<button class="dislikeButton reaction-btn">👎 <span class="dislikeCount">0</span></button>
-				</div>
-			</div>
-			<p class="descripcion-articulo">DDescripciónDescripciiónDescripciónDescripciónDesciónDescripciónDescripciónDesciónDescripciónDescripciónDesciónDescripciónDescripciónDesciónDescripciónDescripciónDesciónDescripciónDescripciónDescónDescripciónDescripciónDescripciónDescripciónDescripciónDescripciónDescripciónDescripciónDescripciónDescripciónDescripciónDescripciónDescripciónDescripciónDescripciónDescripciónDescripciónDescripciónDescripciónDescripciónDescripciónDescripciónDescripciónDescripciónDescripciónDescripciónDescripciónDescripciónDescripciónDescripciónDescripciónDescripciónDescripciónDescripciónescripción</p>
-			<div id="comment-section">
-				<h2>Comentarios</h2>
-				<form id="comment-form">
-					<div class="comment">
-						<img src="Fotoperfilpordefecto.png" alt="Avatar" class="avatar">
-						<textarea id="comment-input" placeholder="Escribe tu comentario aquí..." required></textarea>
-						<button class="form-submit-button" type="submit">Enviar</button>
-					</div>
-				</form>
-				<div id="comments-container">
-					<div class="comment">
-						<img src="Fotoperfilpordefecto.png" alt="Avatar" class="avatar">
-						<div class="comment-content">
-							<p>Este es el ejemplo de un comentario</p>
-							<div class="reaction-buttons-articulo carousel-item">
-								<button class="likeButton reaction-btn-comentarios">👍 <span class="likeCount">0</span></button>
-								<button class="dislikeButton reaction-btn-comentarios">👎 <span class="dislikeCount">0</span></button>
-							</div>
-						</div>
-					</div>
-				</div>
-			</div>
-			<footer>
-				<p>&copy; 2024 Gymanual. Todos los derechos reservados.</p>
-			</footer>
-			<script src="script.js" defer></script>
-		</div>
-	</body>
-</html>
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('articulo_comunidad' , {});
+});
+
+module.exports = router;
