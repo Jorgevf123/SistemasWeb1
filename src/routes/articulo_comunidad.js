bdf8dcfb323f6b6e7cdefa4de68f8098f94f7944
Crramos <carlos.ramosrial@usp.ceu.es>
2024-11-25 23:41:53 +0100

    me falta base de datos y retoques

--- src/routes/articulo_comunidad.js
+++ src/routes/articulo_comunidad.js
@@ -3,7 +3,13 @@ const router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('articulo_comunidad', { title: 'Artículo de la comunidad', user:req.session.user});
+  res.render('articulo_comunidad', { title: 'Artículo de la comunidad',
+                                    user:req.session.user, 
+                                    titulo_articulo:'Título 1', 
+                                    imagen_articulo:'/images/img1.jpg', 
+                                    usuario_escritor:'Usuario 1', 
+                                    descripcion:'Descripcion'
+                                  });
 });
 
 module.exports = router;
