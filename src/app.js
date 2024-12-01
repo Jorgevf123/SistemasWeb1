29478f60fbf3b5549dd9864ea08dfbc7fb1947b1
Juan Antonio Peregrina Peregrina <juanantonio.peregrinaperegrina@usp.ceu.es>
2024-12-01 12:29:46 +0100

    Adaptado el header tanto en contacto como conocenos a la plantilla

--- src/app.js
+++ src/app.js
@@ -100,6 +100,6 @@ app.use(function(err, req, res, next) {
   res.locals.error = req.app.get('env') === 'development' ? err : {};
 // render the error page
   res.status(err.status || 500);
-  res.render('error');
+  res.render('error', { error: err });
 });
 module.exports = app;
--- src/routes/conocenos.js
+++ src/routes/conocenos.js
@@ -1,9 +1,19 @@
-const express = require('express');
+/*const express = require('express');
 const router = express.Router();
 
 // Ruta principal para las rutinas
 router.get('/', (req, res) => {
     res.render('conocenos'); // Renderiza la plantilla `rutina.ejs`
+});
+
+module.exports = router;
+*/
+const express = require('express');
+const router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('conocenos' , { title: 'Conócenos'});
 });
 
 module.exports = router;
