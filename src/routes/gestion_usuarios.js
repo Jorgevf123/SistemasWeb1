c3d68e79f079d7b68fcac7cd8af66701a3d1ce77
Antonio Carrasco <antonio.carrascojuarez@usp.ceu.es>
2024-12-07 21:13:22 +0100

    listo gestion usurios

--- src/routes/gestion_usuarios.js
+++ src/routes/gestion_usuarios.js
@@ -1,11 +1,17 @@
 const express = require('express');
 const router = express.Router();
+const sequelize = require('../sequelize');
 
 // Ruta para gestión de usuarios
 router.get('/', (req, res) => {
-    // Renderiza la vista gestion_usuarios.ejs
-    res.render('gestion_usuarios', { title: 'Gestión de Usuarios' });
+    // Asegúrate de pasar el objeto `user` a la vista
+    const user = req.user || null;  // Si no tienes un usuario, asignas `null`
+    
+    // Renderiza la vista gestion_usuarios.ejs y pasa `user`
+    res.render('gestion_usuarios', { 
+        title: 'Gestión de Usuarios',
+        user: user  // Pasamos la variable user a la vista
+    });
 });
 
 module.exports = router;
-
