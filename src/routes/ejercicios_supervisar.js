fb9da86fcfce9e5c99ee8b87fc5aecdf12e9a4e3
Jorge Angel <jorgeangel.vazquezfernandez@usp.ceu.es>
2024-12-07 21:48:54 +0100

    Cambios realizados de direccionamiento

--- src/routes/ejercicios_supervisar.js
+++ src/routes/ejercicios_supervisar.js
@@ -1,10 +1,16 @@
 const express = require('express');
 const router = express.Router();
+const sequelize = require('../sequelize');
 
 // Ruta para ejercicios a supervisar
 router.get('/', (req, res) => {
+    // Asegúrate de pasar el objeto `user` a la vista
+    const user = req.user || null;  // Si no tienes un usuario, asignas `null`
     // Renderiza la vista ejercicios_supervisar.ejs
-    res.render('ejercicios_supervisar', { title: 'Ejercicios a Supervisar' });
+    res.render('ejercicios_supervisar', { 
+        title: 'Ejercicios a Supervisar',
+        user: user  // Pasamos la variable user a la vista
+    });
 });
 
 module.exports = router;
