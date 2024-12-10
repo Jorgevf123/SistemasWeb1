825928bf5e5cc5cf8ace4835e54ae3bdf60ee374
Juan Antonio Peregrina Peregrina <juanantonio.peregrinaperegrina@usp.ceu.es>
2024-12-10 11:55:47 +0100

    Restringido bien el header botones de invitados

--- src/app.js
+++ src/app.js
@@ -14,23 +14,14 @@ const articulo_comunidad = require('./routes/articulo_comunidad');
 const articulo_comunidad_editable = require('./routes/articulo_comunidad_editable');
 //const concertsRouter = require('./routes/atclients');
 const comunidad = require('./routes/comunidad');
-//const entrada_eventoRouter = require('./routes/contacto');
-//const formularioCompraRouter = require('./routes/editar_perfil');
-//const adminRouter = require('./routes/ejercicios_a_validar');
-//const loginRouter = require('./routes/ejerciicios_supervisar');
-//const registerRouter = require('./routes/ejercicios');
+
 const favoritos = require('./routes/favoritos');
 const apiFavoritos = require('./routes/api/favoritos');
 const gestionUsuariosRouter = require('./routes/gestion_usuarios');
 const ejerciciosSupervisarRouter = require('./routes/ejercicios_supervisar');
 const ejercicioAValidarRouter = require('./routes/ejercicio_a_validar');
-//const editProfileRouter = require('./routes/gestion_usuarios');
-//const myticketsRouter = require('./routes/iniciosesion');
-//const foroRouter = require('./routes/perfil');
-//const foroRouter2 = require('./routes/PerfilAdmin');
-//const foroRouter3 = require('./routes/registro');
-//const foroRouter4 = require('./routes/rutina');
-//const foroRouter5 = require('./routes/terminosycondiciones');
+
+
 const rutinasRouter = require('./routes/rutina');
 const inicioRouter = require('./routes/iniciosesion');
 const registroRouter = require('./routes/registro');
@@ -94,21 +85,7 @@ app.use('/buscar', buscarRouter);
 app.use('/', elegirRoutes);
 app.use('/perfil', perfilRouter);  // Asegúrate de que la ruta /perfil esté registrada
 app.use('/PerfilAdmin', perfiladminRouter);
-//app.use('/otherevents', othereventsRouter);
-//app.use('/formulario_compra', formularioCompraRouter);
-//app.use('/foro', foroRouter);
 
-//app.use('/adminsection', adminRouter);
-//app.use('/login', loginRouter);
-//app.use('/register', registerRouter);
-//app.use('/profile', profileRouter);
-//app.use('/editProfile', editProfileRouter);
-//app.use('/myTickets', myticketsRouter);
-//app.use('/entrada_evento', entrada_eventoRouter);
-//app.use('/logout', (req,res) =>{
-//  req.session.destroy();
-//  res.redirect("/");
-//});
 function restrict(req, res, next) {
   if (req.session.user && (req.session.user.rol === 'user' || req.session.user.rol === 'admin')) {
     next();
@@ -124,17 +101,6 @@ module.exports = { restrict };
 app.use(function(req, res, next) {
   next(createError(404));
 });
-
-// error handler
-/*app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
-// render the error page
-  res.status(err.status || 500);
-  res.render('error', { error: err });
-});
-*/
 
 
 module.exports = app;
