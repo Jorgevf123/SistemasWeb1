4b1c182eb914a706f77b33eb11c32a276c1ea836
Juan Antonio Peregrina Peregrina <juanantonio.peregrinaperegrina@usp.ceu.es>
2024-12-10 10:28:51 +0100

    Arreglado el header de rutinas

--- src/routes/rutina.js
+++ src/routes/rutina.js
@@ -2,8 +2,11 @@ const express = require('express');
 const router = express.Router();
 
 // Ruta principal para las rutinas
-router.get('/', (req, res) => {
-    res.render('rutina'); // Renderiza la plantilla `rutina.ejs`
-});
+/* GET home page. */
+router.get('/', function(req, res, next) {
+    const usuario = req.session.user || null; // Asegúrate de que `user` no sea undefined
+    res.render('rutina' , { title: 'Rutinas', user:usuario, imagen_perfil:usuario ? usuario.imagen_perfil : '/images/avatar.webp'});
+  });
 
 module.exports = router;
+
